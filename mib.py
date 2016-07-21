b6cb055a518c3a9dcd4f17ea88bb29a9f4d05fd0
jazzycamel <jazzycamel@googlemail.com>
2016-07-21 16:06:50 +0100

    Adding content.

---
+++ mib.py
@@ -0,0 +1,36 @@
+from pysnmp.smi import builder, view, compiler, rfc1902
+import os
+
+mb=builder.MibBuilder()
+mibSrcs=mb.getMibSources()+(builder.DirMibSource('/home/pi/.pysnmp/mibs/'),)
+mb.setMibSources(*mibSrcs)
+print(mibSrcs)
+mb.loadModules('SNMPv2-MIB', 'SNMP-COMMUNITY-MIB', 'PowerNet-MIB', 'UPS-MIB')
+vc=view.MibViewController(mb)
+
+varBinds=[
+    ('1.3.6.1.2.1.1.3.0',9041620),
+    ('1.3.6.1.6.3.1.1.4.1.0','1.3.6.1.4.1.318.0.636'),
+    ('1.3.6.1.4.1.318.2.3.3.0',b'UPS: On battery power in response to distorted input.'),
+    ('1.3.6.1.2.1.33.1.2.3.0' ,20),
+    ('1.3.6.1.2.1.33.1.2.2.0', 0),
+    ('1.3.6.1.2.1.33.1.9.7.0', 2),
+
+]
+
+#varBinds=[
+#    for x in varBinds
+#]
+
+#for vb in varBinds: print(vb.prettyPrint())
+
+#print(vc.getNodeName((1,3,6,1,2)))
+for k,v in varBinds:
+    q=tuple(int(kk) for kk in k.split('.'))
+    print(k,q)
+    try:print(vc.getNodeName(q))
+    except: print("Fail 1!")
+    try: 
+        vb=rfc1902.ObjectType(rfc1902.ObjectIdentity(k), v).resolveWithMib(vc)
+        print(vb.prettyPrint())
+    except: print("Fail 2!")
---
+++ snmptrap.py
@@ -0,0 +1,70 @@
+from pysnmp.carrier.asyncore.dispatch import AsyncoreDispatcher
+from pysnmp.carrier.asyncore.dgram import udp, udp6, unix
+from pyasn1.codec.ber import decoder
+from pysnmp.proto import api
+import netifaces as ni
+
+#IPADDR="10.128.235.241"
+#IPADDR="localhost"
+IPADDR=ni.ifaddresses('eth0')[2][0]['addr']
+PORT=162
+
+def toTime(s):
+    d=s/86400
+    s-=(86400*d)
+    h=s/3600
+    s-=(h*3600)
+    m=s/60
+    s-=(60*m)
+    return d,h,m,s
+
+# noinspection PyUnusedLocal
+def cbFun(transportDispatcher, transportDomain, transportAddress, wholeMsg):
+    while wholeMsg:
+        msgVer = int(api.decodeMessageVersion(wholeMsg))
+        if msgVer in api.protoModules:
+            pMod = api.protoModules[msgVer]
+        else:
+            print('Unsupported SNMP version %s' % msgVer)
+            return
+        reqMsg, wholeMsg = decoder.decode(
+            wholeMsg, asn1Spec=pMod.Message(),
+        )
+        print('Notification message from %s:%s: ' % (
+            transportDomain, transportAddress
+        )
+              )
+        reqPDU = pMod.apiMessage.getPDU(reqMsg)
+        if reqPDU.isSameTypeWith(pMod.TrapPDU()):
+            if msgVer == api.protoVersion1:
+                print('Enterprise: %s' % (pMod.apiTrapPDU.getEnterprise(reqPDU).prettyPrint()))
+                print('Agent Address: %s' % (pMod.apiTrapPDU.getAgentAddr(reqPDU).prettyPrint()))
+                print('Generic Trap: %s' % (pMod.apiTrapPDU.getGenericTrap(reqPDU).prettyPrint()))
+                print('Specific Trap: %s' % (pMod.apiTrapPDU.getSpecificTrap(reqPDU).prettyPrint()))
+                print('Uptime: %s' % (pMod.apiTrapPDU.getTimeStamp(reqPDU).prettyPrint()))
+                varBinds = pMod.apiTrapPDU.getVarBindList(reqPDU)
+            else:
+                varBinds = pMod.apiPDU.getVarBindList(reqPDU)
+            print('Var-binds:')
+            for oid, val in varBinds:
+                print('%s = %s' % (oid.prettyPrint(), val.prettyPrint()))
+    return wholeMsg
+
+
+transportDispatcher = AsyncoreDispatcher()
+
+transportDispatcher.registerRecvCbFun(cbFun)
+
+# UDP/IPv4
+transportDispatcher.registerTransport(
+    udp.domainName, udp.UdpSocketTransport().openServerMode((IPADDR, PORT))
+)
+
+transportDispatcher.jobStarted(1)
+
+try:
+    # Dispatcher will never finish as job#1 never reaches zero
+    transportDispatcher.runDispatcher()
+except:
+    transportDispatcher.closeDispatcher()
+    raise
