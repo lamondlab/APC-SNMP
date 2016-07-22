9a12651e0c1e79c319054f0c7239808626e79825
Rob Kent <rob@gulon.co.uk>
2016-07-22 14:01:54 +0100

    Developing snmpget test into heatbeat

--- snmpget.py
+++ snmpget.py
@@ -2,15 +2,30 @@ from pysnmp.entity.rfc3413.oneliner import cmdgen
 
 cmdGen=cmdgen.CommandGenerator()
 
-c1=cmdgen.MibVariable('PowerNet-MIB','upsBasicIdentModel',0).addMibSource('/home/pi/APC-SNMP/CompiledMIBs')
-c2=cmdgen.MibVariable('PowerNet-MIB','upsBasicOutputStatus',0).addMibSource('/home/pi/APC-SNMP/CompiledMIBs')
+#c1=cmdgen.MibVariable('PowerNet-MIB','upsBasicIdentModel',0).addMibSource('/home/pi/APC-SNMP/CompiledMIBs')
+#c2=cmdgen.MibVariable('PowerNet-MIB','upsBasicOutputStatus',0).addMibSource('/home/pi/APC-SNMP/CompiledMIBs')
+
+commands=(
+	# Identity
+	('upsBasicIdentModel',0),
+	('upsBasicOutputStatus',0),
+
+	# Battery
+	('upsBasicBatteryStatus',0),
+	('upsAdvBatteryCapacity',0),
+	('upsAdvBatteryTemperature',0),
+	('upsAdvBatteryRunTimeRemaining',0),
+)
+mibVariables=[]
+for cmd in commands:
+	mibVariables.append(
+		cmdgen.MibVariable('PowerNet-MIB',*cmd).addMibSource('/home/pi/APC-SNMP/CompiledMIBs')
+	)
 
 indication,status,index,varBinds=cmdGen.getCmd(
 	cmdgen.CommunityData('public', mpModel=0),
 	cmdgen.UdpTransportTarget(('134.36.67.93',161)),
-        cmdgen.MibVariable('iso.org.dod.internet.mgmt.mib-2.system.sysDescr.0'),
-        cmdgen.MibVariable('SNMPv2-MIB', 'sysDescr', 0),
-        c1,c2
+	*mibVariables
 )
 
 if indication: print(indication)
