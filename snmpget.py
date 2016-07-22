ebbd3d4ab704fa4e63fc0562851539e75a13dc2a
Rob Kent <rob@gulon.co.uk>
2016-07-22 15:03:17 +0100

    Looping heartbeat

--- snmpget.py
+++ snmpget.py
@@ -1,9 +1,5 @@
+from time import sleep
 from pysnmp.entity.rfc3413.oneliner import cmdgen
-
-cmdGen=cmdgen.CommandGenerator()
-
-#c1=cmdgen.MibVariable('PowerNet-MIB','upsBasicIdentModel',0).addMibSource('/home/pi/APC-SNMP/CompiledMIBs')
-#c2=cmdgen.MibVariable('PowerNet-MIB','upsBasicOutputStatus',0).addMibSource('/home/pi/APC-SNMP/CompiledMIBs')
 
 commands=(
     # Identity
@@ -11,12 +7,12 @@ commands=(
     ('upsBasicOutputStatus',0),
 
     # Battery
-    ('upsBasicBatteryStatus',0),         # str
-    ('upsBasicBatteryTimeOnBattery',0),         # x10-2 (1/100) seconds
-    ('upsHighPrecBatteryCapacity',0),         # %
-    ('upsHighPrecBatteryTemperature',0),      # °C
-    ('upsAdvBatteryRunTimeRemaining',0), # x10-2 (1/100) seconds 
-    ('upsHighPrecBatteryActualVoltage',0),    # V
+    ('upsBasicBatteryStatus',0),           # str
+    ('upsBasicBatteryTimeOnBattery',0),    # x10-2 (1/100) seconds
+    ('upsHighPrecBatteryCapacity',0),      # % *10
+    ('upsHighPrecBatteryTemperature',0),   # °C *10
+    ('upsAdvBatteryRunTimeRemaining',0),   # x10-2 (1/100) seconds 
+    ('upsHighPrecBatteryActualVoltage',0), # V *10
 
     # Input Voltage
     ('upsBasicInputPhase',0),             # 
@@ -36,25 +32,37 @@ commands=(
     ('upsHighPrecOutputEfficiency',0),  # % *10
     ('upsHighPrecOutputEnergyUsage',0), # kWh *10
 )
+
+print("Create MIB Variables...")
 mibVariables=[]
 for cmd in commands:
     mibVariables.append(
         cmdgen.MibVariable('PowerNet-MIB',*cmd).addMibSource('/home/pi/APC-SNMP/CompiledMIBs')
     )
 
-indication,status,index,varBinds=cmdGen.getCmd(
-    cmdgen.CommunityData('public', mpModel=0),
-    cmdgen.UdpTransportTarget(('134.36.67.93',161)),
-    *mibVariables
-)
+def heartbeat():
+    cmdGen=cmdgen.CommandGenerator()
+    indication,status,index,varBinds=cmdGen.getCmd(
+        cmdgen.CommunityData('public', mpModel=0),
+        cmdgen.UdpTransportTarget(('134.36.67.93',161)),
+        *mibVariables
+    )
 
-if indication: print(indication)
-else:
-    if status:
-        print("{} at {}".format(
-            status.prettyPrint(),
-            index and varBinds[int(errorIndex)-1] or '?'
-        ))
+    if indication: print(indication)
     else:
-        for name,val in varBinds:
-            print("{} = {}".format(name.prettyPrint(), val.prettyPrint()))
+        if status:
+            print("{} at {}".format(
+                status.prettyPrint(),
+                index and varBinds[int(errorIndex)-1] or '?'
+            ))
+        else:
+            for name,val in varBinds:
+                print("{} = {}".format(name.prettyPrint(), val.prettyPrint()))
+
+print("Starting...")
+while 1:
+    try:
+        print("Heartbeating...?")
+        heartbeat()
+        sleep(30)
+    except KeyboardInterrupt: break
